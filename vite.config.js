2545cfc4360ca88c3990f1aa465e386ddeca37e1
Ali Yasir Naç <76746351+aliyasirnac@users.noreply.github.com>
2024-11-10 22:52:57 +0300

    base path kaldırıldı

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/boun-course-planner/' : '/',
   plugins: [svelte()]
 })
